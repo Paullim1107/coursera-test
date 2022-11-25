f9f9897aac1eec50719cbf17c2b6728f13fcd598
paullim1107 <paullim1107@gmail.com>
2022-11-25 10:09:02 -0800

    save in progress

---
+++ DIApp.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+  angular.module("DIApp", []).controller("DIController", DIController);
+
+  function DIController($scope, $filter) {
+    $scope.name = "Paul";
+    $scope.upper = function () {
+      var upCase = $filter("uppercase");
+      $scope.name = upCase($scope.name);
+    };
+  }
+})();
